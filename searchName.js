0f82f012a6caac4dbada48f40ec1bf1d21f5fef3
Daniel-Davis <ddv9461@saintjoe.edu>
2015-02-19 07:55:00 -0800

    Update searchName.js

--- searchName.js
+++ searchName.js
@@ -1,2 +1,6 @@
 // Daniel Davis, searchName, searches through storage for a part by its name(reduce basically)//
 
+load('storage.js'); // loads up the storage file including all of the data in it
+
+
+
