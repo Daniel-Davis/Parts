d2a048046314974f4afd774451f0e77506997454
Daniel-Davis <ddv9461@saintjoe.edu>
2015-02-19 07:38:04 -0800

    Update Storage.js

--- Storage.js
+++ Storage.js
@@ -1,4 +1,31 @@
 // Daniel Davis, Storage, loction that holds 25 different electronic parts that i will be keeping in storage//
 
-var storage = [
-  {'part':
+var storage = [ // creating a storage cabinet for electronic parts with name, serial number, quantity, what type it is, and if there is any damag
+  '{ "partName": "wire" : "serialNum": "N/A" : "quantity": 50 : "type": "wire" : "damage": false }',
+  '{ "partName": "toggleSwtich" : "serialNum": "N/A" : "quantity": 10 : "type":  "switch" : "damage": false }',
+  '{ "partName": "pushButtonSwitch" : "serialNum": "N/A" : "quantity": 10 : "type": "switch" : "damage": false }',
+  '{ "partName": "dipSwitch" : "serialNum": "N/A" : "quantity": 10 : "type": "switch" : "damage": false }',
+  '{ "partName": "resistor" : "serialNum": "N/A" : "quantity": 50 : "type": "resistor" : "damage": false }',
+  '{ "partName": "variableResistor" : "serialNum": "N/A" : "quantity": 50 : "type": "resistor" : "damage": false }',
+  '{ "partName": "capacitor" : "serialNum": "N/A" : "quantity": 25 : "type": "capacitor" : "damage": false }',
+  '{ "partName": "variableCapacitor" : "serialNum": "N/A" : "quantity": 25 : "type": "capacitor" : "damage": false }',
+  '{ "partName": "electrolyticCapacitor" : "serialNum": "N/A" : "quantity": 25 : "type":  "capacitor" : "damage": false }',
+  '{ "partName": "bjtTransistor" : "serialNum": "N/A" : "quantity": 20 : "type": "transistor" : "damage": false }',
+  '{ "partName": "mosTransistor" : "serialNum": "N/A" : "quantity": 20 : "type": "transistor" : "damage": false }',
+  '{ "partName": "relay" : "serialNum": "N/A" : "quantity": 25 : "type": "relay" : "damage": false }',
+  '{ "partName": "jumper" : "serialNum": "N/A" : "quantity": 25 : "type": "jumper" : "damage": false }',
+  '{ "partName": "diode" : "serialNum": "N/A" : "quantity": 25 : "type": "diode" : "damage": false }',
+  '{ "partName": "inductor" : "serialNum": "N/A" : "quantity": 10 : "type": "inductor" : "damage": false }',
+  '{ "partName": "battery" : "serialNum": "N/A" : "quantity": 25 : "type": "battery" : "damage": false }',
+  '{ "partName": "lightBulb" : "serialNum": "N/A" : "quantity": 10 : "type": "light" : "damage": false }',
+  '{ "partName": "antenna" : "serialNum": "N/A" : "quantity": 10 : "type": "antenna" : "damage": false }',
+  '{ "partName": "fuse" : "serialNum": "N/A" : "quantity": 25 : "type": "fuse" : "damage": false }',
+  '{ "partName": "buzzer" : "serialNum": "N/A" : "quantity": 10 : "type":  "sound" : "damage": false }',
+  '{ "partName": "speaker" : "serialNum": "N/A" : "quantity": 10 : "type": "sound" : "damage": false }',
+  '{ "partName": "microphone" : "serialNum": "N/A" : "quantity": 10 : "type": "sound" : "damage": false }',
+  '{ "partName": "transformer" : "serialNum": "N/A" : "quantity": 20 : "type": "transformer" : "damage": false }',
+  '{ "partName": "crystalOscillator" : "serialNum": "N/A" : "quantity": 15 : "type": "oscillator" : "damage": false }',
+  '{ "partName": "electricMotor" : "serialNum": "N/A" : "quantity": 30 : "type": "motor" : "damage": false }'
+  ].join(",/n ") + "/n]";// end of storage
+  
+  
