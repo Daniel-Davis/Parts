8b8da63007dd873714aef568d1643e969d3ca80b
Daniel-Davis <ddv9461@saintjoe.edu>
2015-02-28 19:38:57 -0800

    Create Map.js

---
+++ Map.js
@@ -0,0 +1,7 @@
+//Daniel Davis, Map, Map function that 
+
+load('storage.js'); // imports storage 'class'
+
+var nameList = JSON.parse(storage); // nameList now understands storage code locically
+
+
