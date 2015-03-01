1f55bcf05e456ca14a27e6b825b54333056e6698
Daniel-Davis <ddv9461@saintjoe.edu>
2015-03-01 07:44:09 -0800

    Update Map.js

--- Map.js
+++ Map.js
@@ -1,25 +1,9 @@
 //Daniel Davis, Map, Map function that alphabetizes
 
 load('storage.js'); // imports storage 'class'
-var itemList = JSON.parse(storage); // nameList now understands storage code locically
+var inventory = JSON.parse(storage); // nameList now understands storage code locically
 
-function alphabetize(array) { // starts function alphabetize
-  for(var i=0, i <= array.length), ++i) { // goes through all of the elements of array
-    if ('partName' != [i]) { // if not an element
-      console.log('Not a valid element'); // throws error message
-      }// end of if statement
-    else { // otherwise ...
-      for (var i=0, i <= string.length, ++i) { // loops through the length of the name
-        
-        
-        
-      }// end of for loop
-    
-    
-    }// end of else statement
-  } // end of for loop
-}// end of alphabetize
-
-
-
-nameList.map.call(str, aplhabetize()).reverse().join(''); 
+print(JSON.stringify(inventory.map(function(array) { // starts print
+  array.sort(); // will sort the array alphabetically
+  return array; // returns the array sorted
+}) ) ); // end of print 
